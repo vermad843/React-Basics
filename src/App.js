74a246681e755fd9a6d0d0aa5b7b6ec0f9c39d8e
vermad843 <vishnuverma7524@gmail.com>
2020-05-28 18:10:28 +0530

    simple react app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,44 @@ import React, { Component } from 'react'
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      age : 21
+    }
+  }
+ 
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age : ++this.state.age
+    })
+  }
+
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age : --this.state.age
+    })
+  }
+
   render() {
+    const {age} = this.state;
     return (
-       <h1>
-         hello world
-       </h1>
+       <div className = "App">
+          <div>
+             Age : 
+             <span> 
+                 {age}
+             </span>
+          </div>
+          <button onClick = {this.onAgeUp}>
+             Age Up
+          </button>
+          <button onClick = {this.onAgeDown}>
+             Age down
+          </button>
+        </div>
     );
   }
 }
