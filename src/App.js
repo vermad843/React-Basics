6fb72161f0cb6d25e5829077171e341ff5ea410d
vermad843 <vishnuverma7524@gmail.com>
2020-07-02 16:32:09 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import './App.css';
 
 
 class App extends Component {
- 
-  render() {
-    const {age, history} = this.props;
-    return (
-       <div className = "App">
-          <div>
-             Age : 
-             <span> 
-                 {age}
-             </span>
-          </div>
-          <button onClick = {this.props.onAgeUp}>
-             Age Up
-          </button>
-          <button onClick = {this.props.onAgeDown}>
-             Age down
-          </button>
-          <br/>
-          <div>
-            History
-          </div>
-          <div>
-             <ul>
+    state = {
+        eligible : false
+    }
+
+    handleChange = (e) => {
+      this.setState({
+          eligible : !this.state.eligible
+       })
+    }
+
+
+    render() {
+        const{eligible} = this.state;
+        return (
+            <> 
                {
-                 history.map((el,i) => {
-                   return (
-                    <li className = "historyItem" key = {i} >
-                     {el.age}
-                    </li>
-                   );
-                 })
-               }
-             </ul>
-          </div>
-        </div>
-    );
-  }
+                   eligible ?  (<div>
+                        <h1>Eligibility</h1></div>)
+                :  (<div>
+                   <h1>Header</h1>
+                    </div>)
+               }     
+               <form>
+                   <input
+                     name = "firstName"
+                     type = "text"
+                   />
+               </form>
+               <div>
+                   <button
+                       onClick = {this.handleChange}
+                      >
+                       Check
+                   </button>
+               </div>
+            </>
+        );
+    }
 }
 
-
-//making state available to components
-const mapStateToProps = (state) => {
-  return {
-    age : state.age,
-    history : state.history
-  } 
-}
-
-
-// converting methods into actions 
-// actions have a type and a payload(what needs to be changed)
-
-const mapDispachToProps = (dispach) => {
-  return { 
-    onAgeUp :  () => dispach({type : 'AGE_UP', value : 1}),
-    onAgeDown :  () => dispach({type : 'AGE_DOWN' , value : 1}),
-  }
-}
-
-export default connect(mapStateToProps,mapDispachToProps)(App);
+export default App;
